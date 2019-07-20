9cad0050b518c739c83dd69c120b735809db54fa
amoda-sasmitha <amoda29@gmail.com>
2019-07-20 12:43:04 +0530

    Mobile Application Development Labsheet 02

--- app/src/main/java/com/example/student/usingstrings/ActivityOne.java
+++ app/src/main/java/com/example/student/usingstrings/ActivityOne.java
@@ -4,11 +4,19 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+
 
 public class ActivityOne extends AppCompatActivity {
 
     TextView txtView2;
+    TextView today;
     int count;
 
     @Override
@@ -16,14 +24,31 @@ public class ActivityOne extends AppCompatActivity {
         Log.i( "LifeCycle", "onCreate Invoked");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+        count = 0;
+
+        today = findViewById(R.id.textView2);
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        today.setText( df.format(c));
+
+
 
         txtView2 = findViewById(R.id.textView3 );
-        txtView2.setText(R.string.msg2);
+        txtView2.setText(String.format("%02d", count));
         txtView2.setTextColor(Color.parseColor("#ffffff"));
-
-        txtView2.setTextSize(25);
         txtView2.setBackgroundColor(getResources().getColor(R.color.JustBlue));
+
+        txtView2.setTextSize(50);
         txtView2.setPadding(30,5,30,5);
+
+        Button btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    count++;
+                txtView2.setText(String.format("%02d", count));
+                }
+        });
 
     }
 
@@ -54,6 +79,8 @@ public class ActivityOne extends AppCompatActivity {
 
     public void onStop(){
         super.onStop();
+//        count = 0;
+//        txtView2.setText(String.format("%02d", count));
         Log.i("LifeCycle","onStop Invoked");
     }
 
@@ -62,6 +89,7 @@ public class ActivityOne extends AppCompatActivity {
         super.onDestroy();
         Log.i("LifeCycle","onDestroy Invoked");
     }
+
 
 
 }
